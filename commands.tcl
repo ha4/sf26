2916652f2fc825a4e2b6fc38df5a43604b898991
ha4 <fkufa@inbox.ru>
2017-05-25 18:22:13 +0500

    visual changed

--- commands.tcl
+++ commands.tcl
@@ -2,86 +2,94 @@
 # -- PROGRAM COMMANDS
 
 proc cmd_about {} {
-  global sf26cc_version
-  global sf26cc_date
-  tk_messageBox -message "SF-26 Data Acustion System\nver $sf26cc_version at $sf26cc_date" -type ok -title "SF-26"
+	global sf26cc_version
+	global sf26cc_date
+
+	tk_messageBox -message "SF-26 Data Acustion System\nver $sf26cc_version\
+ at $sf26cc_date" -type ok -title "SF-26"
 }
 
 proc cmd_cons {} {
-catch {console show}
+	catch {console show}
 }
 
 proc cmd_exit {} {
-  global config_file config_vars
-  set retc [tk_messageBox -message "Really exit?" -type yesno -icon warning -title "SF-26 Exit"]
-  if {$retc == yes} {
-     config_save $config_file $config_vars
-     exit
-  }
+	global config_file config_vars
+
+	set retc [tk_messageBox -message "Really exit?" \
+		-type yesno -icon warning -title "SF-26 Exit"]
+
+	if {$retc == yes} {
+		config_save $config_file $config_vars
+		chu close
+		exit
+	}
 }
 
 proc cmd_clear {} {
-        global StartT
-	::AutoPlotM::clear .t.c
+	global StartT
+	::AutoPlotM::clear .c
 	unset -nocomplain StartT
 }
 
 proc cmd_close {} {
-   global  dumpfl
+	global  dumpfl
 
-   set m $dumpfl
-   unset dumpfl
-   close $m
-   .toolbar.open configure -text "  Open"
+	set m $dumpfl
+	unset dumpfl
+	close $m
+
+	setbutton "Recording"
 }
 
 proc cmd_open {} {
+	global  dumpfl
+	global  config_logfile
 
-   global  dumpfl
-   global  config_logfile
+	if {[info exist dumpfl]} {
+		cmd_close
+		return
+	}
 
-   if {[info exist dumpfl]} {
-	cmd_close
-	return
-   }
+	if {[file exist $config_logfile]} {
+		set retc [tk_messageBox -message "File EXIST.\n Overwrite??" \
+			-type yesno -icon warning -title "Data File Overwrite"]
+	if { $retc != "yes" } { return }
+	}
 
-   if {[file exist $config_logfile]} {
-     set retc [tk_messageBox -message "File EXIST.\n Overwrite??" -type yesno -icon warning -title "Data File Overwrite"]
-     if { $retc != "yes" } { return }
-   }
-
-   set dumpfl [open $config_logfile w+]
-   .toolbar.open configure -text "  Close"
+	set dumpfl [open $config_logfile w+]
+	setbutton "Stop Rec."
 }
 
 proc cmd_clr {} {
-    global  n
-    set n 0
-    ::AutoPlotM::clear .t.c
-	animate .toolbar.anim
+	global  n
+	set n 0
+
+	::AutoPlotM::clear .c
+	animate
 }
 
 proc cmd_cell {} {
-    unset -nocomplain fil1
-
+	 unset -nocomplain fil1
 }
 
 proc cmd_conn {} {
-   global config_port
+	global config_port
 
-   chu port $config_port
-   chu restart
+	chu port $config_port
+	chu restart
 }
 
 proc cmd_fsel {fvar} {
 	upvar #0 $fvar sf
 	set types {
-	    {{Data Files}       {.sf.dat}        }
-	    {{Text Files}       {.txt}        }
-	    {{All Files}        *             }
+		{{Data Files}       {.sf.dat}     }
+		{{Text Files}       {.txt}        }
+		{{All Files}        *             }
 	}
 
-	set filename [tk_getSaveFile -filetypes $types -defaultextension {.sf.dat}]
+	set filename [tk_getSaveFile -filetypes $types \
+		-defaultextension {.sf.dat}]
 
 	if { $filename != "" } { set sf $filename }
 }
--- visual.tcl
+++ visual.tcl
@@ -6,8 +6,8 @@ proc hr {w} {frame $w -height 2 -borderwidth 1 -relief sunken}
 proc frames {} {
 global chart
 wm title . "SF-26"
+wm protocol . WM_DELETE_WINDOW { .mbar.fl invoke Exit }
 menu  .mbar
-frame .t
 . configure -menu .mbar
 
 menu .mbar.fl -tearoff 0
@@ -35,6 +35,8 @@ menu .mbar.dat -tearoff 0
 .mbar.dat add checkbutton -label "Auto corrections" -onvalue 1 -offvalue 0 -variable dataCORR
 
 frame .toolbar -bd 2 -relief flat
+canvas .c -relief groove -bg beige
+frame .toolbarl -bd 2 -relief flat
 
 
 # An exit button with an image is created.
@@ -42,30 +44,42 @@ frame .toolbar -bd 2 -relief flat
 # button .toolbar.exitButton -image img -relief flat -command {cmd_exit}
 
 # An exit button with a text
-button .toolbar.conn  -text "Connect" -relief flat -overrelief raised -command {cmd_conn}
-entry  .toolbar.port                -relief sunken                 -textvariable config_port -width 10
+button .toolbarl.conn  -text "Connect" -borderwidth 1 -relief flat -overrelief raised -command {cmd_conn}
+entry  .toolbarl.port                -relief sunken                 -textvariable config_port -width 10
 
-label  .toolbar.dset1  -relief flat -text "1" -state disabled
-label  .toolbar.dset2  -relief flat -text "2" -state disabled
-label  .toolbar.dset3  -relief flat -text "3" -state disabled
-label  .toolbar.dset4  -relief flat -text "4" -state disabled
-entry  .toolbar.dvolt  -relief sunken -textvariable datavolt -width 10
+label  .toolbarl.dset1  -relief flat -text "1" -state disabled
+label  .toolbarl.dset2  -relief flat -text "2" -state disabled
+label  .toolbarl.dset3  -relief flat -text "3" -state disabled
+label  .toolbarl.dset4  -relief flat -text "4" -state disabled
+entry  .toolbarl.dvolt  -relief sunken -textvariable datavolt -width 10
 
-button .toolbar.open  -text "  Open" -relief flat -overrelief raised -command {cmd_open}
+label  .toolbar.l1 -text "IN"
+entry  .toolbar.din   -relief sunken -textvariable dataDi -width 10
+label  .toolbar.l2 -text "OUT"
+entry  .toolbar.dout  -relief sunken -textvariable dataDo -width 10
+
+button .toolbar.open  -text "Record" -relief raised -overrelief raised -command {cmd_open}
 label  .toolbar.anim  -relief flat
 entry  .toolbar.file                -relief sunken                 -textvariable config_logfile -width 26
 button .toolbar.fsel  -text "..."   -relief raised                 -command {cmd_fsel config_logfile}
 checkbutton .toolbar.shex -text "Show Data" -relief flat -variable vShowEx -command {showex $vShowEx}
 
-pack   .toolbar.conn  -side left
-pack   .toolbar.port  -side left
-pack   .toolbar.dset1  -side left
-pack   .toolbar.dset2  -side left
-pack   .toolbar.dset3  -side left
-pack   .toolbar.dset4  -side left
-pack   .toolbar.dvolt  -side left
+pack   .toolbarl.conn  -side left
+pack   .toolbarl.port  -side left
+pack   .toolbarl.dset1  -side left
+pack   .toolbarl.dset2  -side left
+pack   .toolbarl.dset3  -side left
+pack   .toolbarl.dset4  -side left
+pack   .toolbarl.dvolt  -side left
 
-pack   .toolbar.open  -side left -padx 2 -pady 2
+pack    [label .toolbar.s1 -text {} -borderwidth 0 -width 2 -padx 0] -side left
+pack   .toolbar.l1  -side left
+pack   .toolbar.din -side left
+pack    [label .toolbar.s2 -text {} -borderwidth 0 -width 2 -padx 0] -side left
+pack   .toolbar.l2  -side left
+pack   .toolbar.dout -side left
+pack    [label .toolbar.s3 -text {} -borderwidth 0 -width 2 -padx 0] -side left
+pack   .toolbar.open  -side left -ipadx 10 -padx 4 -anchor center
 pack   .toolbar.anim  -side left
 pack   .toolbar.file  -side left
 pack   .toolbar.fsel  -side left
@@ -75,9 +89,11 @@ pack   .toolbar.shex  -side left
 # pack   .toolbar.clrButton  -side left -padx 2 -pady 2
 # pack   .toolbar.consButton -side left -padx 2 -pady 2
 
-pack .toolbar -fill x -expand false
 
-canvas .t.c -relief groove -bg beige
+pack .toolbar -fill x -expand false
+pack .c -side top -fill both -expand true
+pack .toolbarl -fill x -expand false
+
 
 # -xscrollincrement 1
 #    -scrollregion {0 0 80000 0} 
@@ -94,21 +110,14 @@ canvas .t.c -relief groove -bg beige
 # grid .t.xscroll x          -sticky news
 
 
-set chart [::AutoPlotM::create .t.c]
+set chart [::AutoPlotM::create .c]
 set ::AutoPlotM::plotcols(setnone)  black
 set ::AutoPlotM::plotcols(setsrcin)  darkgreen
 set ::AutoPlotM::plotcols(setsrccal)  darkblue
 set ::AutoPlotM::plotcols(setsrcout)  darkred
 set ::AutoPlotM::plotcols(setsrcd)  darkgrey
 
-grid .t.c -sticky news
-grid rowconfigure    .t 0 -weight 1
-grid columnconfigure .t 0 -weight 1
-
-pack .t -side top -fill both -expand true
-
-setanimate .toolbar.anim {gray12 gray50 gray75 gray50}
-wm protocol . WM_DELETE_WINDOW { .mbar.fl invoke Exit }
+setanimate .toolbar.anim {gray12 gray50 gray75 gray50} 1
 
 
 toplevel .toolbar2 -bd 2 -relief flat
@@ -134,19 +143,19 @@ entry  .toolbar2.vToutmax -relief sunken -textvariable par_tocorr -width 6
 entry  .toolbar2.vAlpha   -relief sunken -textvariable par_alpha -width 5
 
 # The toolbar is packed to the root window. It is horizontally stretched.
-grid   [label .toolbar2.lp1 -an center -text "Current values"] - -sticky ew -padx 4
+grid   [label .toolbar2.lp1 -an center -text "Current values"] - -sticky w -padx 4
 grid   [label .toolbar2.lTm -an e -text "time"] .toolbar2.vTm -sticky ew -padx 4
 grid   [label .toolbar2.lDi -an e -text "Din"] .toolbar2.vDin -sticky ew -padx 4
 grid   [label .toolbar2.lDo -an e -text "Dout"] .toolbar2.vDout -sticky ew -padx 4
 grid   [hr .toolbar2.hr1] - -sticky ew -padx 4 -pady 5
-grid   [label .toolbar2.lp2 -an center -text "Current parameters"] - -sticky ew -padx 4
+grid   [label .toolbar2.lp2 -an center -text "Current parameters"] - -sticky w -padx 4
 grid   [label .toolbar2.lTd -an e -text "T% dark"] .toolbar2.vTd  -sticky ew -padx 4
 grid   [label .toolbar2.lTi -an e -text "Tin%"]     .toolbar2.vTin -sticky ew -padx 4
 grid   [label .toolbar2.lTo -an e -text "Tout%"]     .toolbar2.vTout -sticky ew -padx 4
 grid   [label .toolbar2.lTc -an e -text "T% corr"] .toolbar2.vTc  -sticky ew -padx 4
 grid   [label .toolbar2.lTcc -an e -text "T%koeff"] .toolbar2.vCk -sticky ew -padx 4
 grid   [hr .toolbar2.hr2]  - -sticky ew -padx 4 -pady 5
-grid   [label .toolbar2.lp3 -an center -text "Setup parameters"] - -sticky ew -padx 4
+grid   [label .toolbar2.lp3 -an center -text "Setup parameters"] - -sticky w -padx 4
 grid   [label .toolbar2.lsk -an e -text "Skip samples"] .toolbar2.nSk -sticky ew -padx 4
 grid   [label .toolbar2.lSd -an e -text "Dark src"] .toolbar2.nSd  -sticky ew -padx 4
 grid   [label .toolbar2.lSi -an e -text "IN src"]  .toolbar2.nSin  -sticky ew -padx 4
@@ -168,36 +177,39 @@ grid columnconfigure    .toolbar2 1 -weight 1
 # -- WINDOWS animation
 
 
-proc animate {w {divisor 4}} {
-    global AnimateImg
+proc animate {} {
+	global AnimImg
 
-    incr AnimateImg($w,n)
-    if {$AnimateImg($w,n) > $divisor} {
-	set AnimateImg($w,n) 0
-    } else {return}
-    set i $AnimateImg($w,idx)
-    $w configure -bitmap [lindex $AnimateImg($w,icons) $i]
-    set AnimateImg($w,idx) [expr ($AnimateImg($w,idx) + 1) % 4]
+	foreach {w _} [split [array names AnimImg *,n] ,] {break}
+	if {[incr AnimImg($w,n)] < $AnimImg($w,div)} {return}
+	set AnimImg($w,n) 0
+
+	if {[incr AnimImg($w,i)] >= $AnimImg($w,ni)} {set AnimImg($w,i) 0}
+	$w configure -bitmap [lindex $AnimImg($w,icons) $AnimImg($w,i)]
 }
 
 
-proc setanimate {w icons} {
-    global AnimateImg
+proc setanimate {w icons {divisor 4}} {
+	global AnimImg
 
-    set AnimateImg($w,idx) 0
-    set AnimateImg($w,n) 0
-    foreach bm $icons {
-	lappend AnimateImg($w,icons) $bm
-    }
-    $w configure -bitmap [lindex $AnimateImg($w,icons) 0]
+	set AnimImg($w,ni) 0
+	set AnimImg($w,div) $divisor
+	set AnimImg($w,n) $divisor
+
+	foreach bm $icons {
+		lappend AnimImg($w,icons) $bm
+		incr AnimImg($w,ni)
+	}
+	set AnimImg($w,i) $AnimImg($w,ni)
+	animate
 }
 
 # activate one of  four flags
 proc show_dset {n} {
-	if {$n == 1} { .toolbar.dset1 configure -state active } else { .toolbar.dset1 configure -state disabled  }
-	if {$n == 2} { .toolbar.dset2 configure -state active } else { .toolbar.dset2 configure -state disabled  }
-	if {$n == 3} { .toolbar.dset3 configure -state active } else { .toolbar.dset3 configure -state disabled  }
-	if {$n == 4} { .toolbar.dset4 configure -state active } else { .toolbar.dset4 configure -state disabled  }
+	if {$n == 1} { .toolbarl.dset1 configure -state active } else { .toolbarl.dset1 configure -state disabled  }
+	if {$n == 2} { .toolbarl.dset2 configure -state active } else { .toolbarl.dset2 configure -state disabled  }
+	if {$n == 3} { .toolbarl.dset3 configure -state active } else { .toolbarl.dset3 configure -state disabled  }
+	if {$n == 4} { .toolbarl.dset4 configure -state active } else { .toolbarl.dset4 configure -state disabled  }
 }
 
 proc showex {showit} {
@@ -216,9 +228,16 @@ proc showex {showit} {
 }
 
 proc showstatus {s} {
-  if {$s != "connected"} {
-	.toolbar.port configure -bg pink
-  } else {
-	.toolbar.port configure -bg [. cget -background]
-  }
+	if {$s != "connected"} {
+		.toolbarl.port configure -bg pink
+	} else {
+		.toolbarl.port configure -bg [. cget -background]
+	}
+}
+
+proc setbutton {s} {
+	.toolbar.open configure -text $c
+}
+
+proc inputdata {s} {
 }
